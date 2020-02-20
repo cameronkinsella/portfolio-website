6ca5642d163e08a56bee796d98a7343b44eba3ac
Cameron Kinsella <53635597+cameronkinsella@users.noreply.github.com>
2020-02-20 00:51:39 -0500

    added some buttons and configured CircleCI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ScrollAnimation from 'react-animate-on-scroll';
+import 'animate.css/animate.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import './App.scss';
+import { SocialIcon } from './components/SocialIcon';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
+    <div className={'App'}>
+
+      <header className={'App-header'}>
+        <h1 style={{ fontWeight: 200 }}>Cameron Kinsella</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Software developer, <br/>
+          UWaterloo Mechatronics Engineering
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <div className={'icon'}>
+          <FontAwesomeIcon icon={faGithub} size={'2x'} className={'github'}/>
+          <SocialIcon icon={'linkedin'}/>
+          <SocialIcon icon={'email'}/>
+          <SocialIcon icon={'resume'}/>
+        </div>
+        <FontAwesomeIcon icon={faChevronDown} className={'chevron'}/>
       </header>
+      <body className={'App-body'}>
+      <ScrollAnimation animateIn='fadeInLeft' animateOut='fadeOutRight'>
+        Some Text
+      </ScrollAnimation>
+      </body>
+
     </div>
+
   );
 }
 
