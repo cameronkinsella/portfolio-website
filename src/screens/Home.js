a30153e257395a7dbb1bf532977aab692f34d693
Cameron Kinsella <53635597+cameronkinsella@users.noreply.github.com>
2020-03-06 23:19:21 -0500

    main screen done (?), still broken on mobile though

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,18 +2,24 @@ import React, { useRef } from 'react';
 import '../App.scss';
 import ScrollAnimation from 'react-animate-on-scroll';
 import 'animate.css/animate.min.css';
+import { useHistory } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { faChevronDown, faCode } from '@fortawesome/free-solid-svg-icons';
 import { SocialIcon } from '../components/SocialIcon';
 
+import me from '../assets/me.jpg'
 
 function Home() {
-  const myRef = useRef(null);
-  const executeScroll = () => scrollToRef(myRef);
+  const aboutMe = useRef(null);
+  const executeScroll = () => scrollToRef(aboutMe);
+  let history = useHistory();
+
+  const goToProjects = () => {
+    history.push("/projects");
+  };
 
   return (
-
     <div className={'App'}>
 
       <header className={'App-header'}>
@@ -22,7 +28,6 @@ function Home() {
           Software Developer, <br/>
           UWaterloo Mechatronics Engineering
         </p>
-
         <div className={'icon'}>
           <a title={'Github: cameronkinsella'} href={'https://github.com/cameronkinsella'}>
             <FontAwesomeIcon icon={faGithub} size={'2x'} className={'github'}/>
@@ -33,23 +38,56 @@ function Home() {
           <a title={'Email: ckinsella@uwaterloo.ca'} href={'mailto:ckinsella@uwaterloo.ca'}>
             <SocialIcon icon={'email'}/>
           </a>
-          <SocialIcon icon={'resume'}/>
+          <a title={'Resume'} href={'https://storage.googleapis.com/personal-website-files/CameronKinsellaResume.pdf'}>
+            <SocialIcon icon={'resume'}/>
+          </a>
         </div>
         <div onClick={executeScroll}>
-          <FontAwesomeIcon icon={faChevronDown} className={'chevron'}/>
+          <FontAwesomeIcon icon={faChevronDown} className={'chevron1'}/>
         </div>
       </header>
-      <main className={'App-body'}>
-      <div className={'header'} ref={myRef}>
-        <ScrollAnimation animateIn='fadeInDown' animateOut='fadeOut'>
-          projects
+      <body className={'App-body'}>
+      <div className={'header1'} ref={aboutMe}>
+        <ScrollAnimation animateIn={'fadeInLeft'} animateOnce={true}>
+          About Me
         </ScrollAnimation>
       </div>
-      <ScrollAnimation animateIn='fadeIn' animateOut='fadeOut'>
-        Cool projects here
-      </ScrollAnimation>
-      </main>
-
+      <img className={'portrait'} src={me} alt={'me'}/>
+      <h2 style={{ fontWeight: 200 }}>Hello there, I'm Cameron</h2>
+      <div className={'bodyContent'}>
+        <ScrollAnimation animateIn={'fadeIn'}>
+          <div className={'bodyBlock'}>
+            I'm a Mechatronics Engineering student at the
+            University of Waterloo with a passion for Software Development.
+            I'm also a pilot! Aircraft I've flown include: <br/>
+            Schweizer SGS 2-33As, Cessna 152s, and Cessna 172s.
+          </div>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn={'fadeIn'}>
+          <div className={'bodyBlock'}>
+            My favorite hobby is programming!
+            I like to learn by working on projects in my free time.
+            Through my personal projects, I've gained experience in a
+            variety of languages including:<br/>
+            <b>Python, C++, Java, Javascript (ES6), HTML5, and CSS</b><br/>
+            along with several framework, including:<br/>
+            <b>PyTorch, OpenCV, React, React Native, and Selenium</b>
+          </div>
+        </ScrollAnimation>
+      </div>
+      <div className={'App-body'}>
+        <div className={'header2'}>
+          <ScrollAnimation animateIn={'fadeInLeft'} animateOnce={true}>
+            Projects
+          </ScrollAnimation>
+        </div>
+        <h2 className={'projectsHeader'}>Check out my projects!</h2>
+        <ScrollAnimation animateIn={'bounce'}>
+          <FontAwesomeIcon icon={faChevronDown} className={'chevron2'}/>
+        </ScrollAnimation>
+        <FontAwesomeIcon onClick={goToProjects} icon={faCode} size={'5x'} className={'code'}/>
+      </div>
+      </body>
     </div>
 
   );
---
+++ src/screens/Projects.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+function Projects() {
+
+  return (
+    <div className={'App'}>
+      <header className={'App-header'}>
+        <h1 style={{ fontWeight: 200 }}>Projects</h1>
+        <p>
+          List of projects here
+        </p>
+        </header>
+    </div>
+  );
+}
+
+export default Projects
