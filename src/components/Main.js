af5afcd03f96e00e2cb07f92a4d01e15ada87a4c
Cameron Kinsella <53635597+cameronkinsella@users.noreply.github.com>
2020-03-06 23:19:21 -0500

    main screen done (?), still broken on mobile though

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Home from '../screens/Home';
+import Projects from '../screens/Projects';
 import NotFound from '../screens/404';
 
 const Main = () => {
   return (
     <Switch>
       <Route exact path={'/'} component={Home}/>
+      <Route exact path={'/projects'} component={Projects}/>
       <Route path={'*'} component={NotFound}/>
     </Switch>
   );
